b40731018ffd7cc437a7eef3a474b1190eaf0374
ShivaayS20 <shivamshishodia2004@gmail.com>
2025-07-14 17:24:27 +0530

    Your commit message here

---
+++ src/index.js
@@ -0,0 +1,56 @@
+//equire('dotenv').config(path: './env')
+
+/*import mongoose from "mongoose";
+import { DB_NAME } from "./constants"; */
+import dotenv from"dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    paht: './env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+import express from "express";
+ const app= express()
+
+(async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("error: ", error);
+                    throw error
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log("app is litening  on port ${process.env.PORT}")
+        })
+
+    } catch (error) {
+        console.error("ERROR: ",error)
+        throw err
+    }
+})()  
+    
+*/
